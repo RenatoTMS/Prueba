b6db578387da594e98032870812092ed825bcf81
Renato <rmunizaga@gmail.com>
2021-08-09 18:45:43 -0400

    pagina Final

--- assests/js/script.js
+++ assests/js/script.js
@@ -11,25 +11,3 @@ $(function(){
     $('.parrafo3').css('color','red');
         });
 });
-var nav=document.querySelector('nav');
-window.addEventListener('scroll',function(){
-    if (window.pageYOffset>173){
-        nav.classList.add('bg-dark','shadow');
-    }else{
-        nav.classList.remove('bg-dark','shadow');
-    }
-});
-
-$(function){
-    $("a").click(function(event){
-        if (this.hash !== ""){
-        event.preventdefault();
-        var gato = this.hash;
-
-    $("html, body").animate({
-        srollTop: $(gato).offset().srollTop 
-    }, 800, function(){
-        window.location.hash = gato;
-        });
-    }
-});
